b75178bec01d5953f8dce61f0c910dc1326dab11
“salmonbek” <salmonlearner34@gmail.com>
2023-12-02 07:25:38 +0500

    masalalar

---
+++ js/main.js
@@ -0,0 +1,203 @@
+// // begin1
+// let a = +prompt("a=");
+// let b = +prompt("b=");
+// s = a * b;
+// p = 2 * (a + b);
+// console.log(s);
+// console.log(p);
+
+// // begin2;
+// let d = +prompt("d=");
+// const pi = 3.14;
+// const r = d / 2;
+// let s = pi * r ** 2;
+// console.log(s);
+
+// // begin3;
+// let a = +prompt("a=");
+// let v = a ** 3;
+// let s = 6 * a ** 2;
+// console.log(v);
+// console.log(s);
+
+// // begin4
+// let a = +prompt("a=");
+// let b = +prompt("b=");
+// let c = +prompt("c=");
+// let v = a * b * c;
+// let s = 2 * (a * b + b * c + a * c);
+// console.log(v);
+// console.log(s);
+
+// begin5
+// let a = +prompt("a=");
+// let b = +prompt("b=");
+// let M = (a + b) / 2;
+// console.log(M);
+
+// // // begin6
+// let a = +prompt("a=");
+// let b = +prompt("b=");
+// let c = (a ** 2 + b ** 2) ** (1 / 2);
+// let p = a + b + c;
+// console.log(c);
+// console.log(p);
+
+// begin7
+// let r1 = +prompt("r1=");
+// let r2 = +prompt("r2=");
+// const pi = 3.14;
+// let s1 = pi * r1;
+// let s2 = pi * r2;
+// let s = r1 ** 2 - r2 ** 2;
+// console.log(s);
+
+// begin8
+// let diametr = +prompt("diametr=");
+// let r = 2 / d;
+// let pi = 3.14;
+// let l = 2 * pi * r;
+// let s = pi * r ** 2;
+// console.log(r);
+// console.log(l);
+// console.log(s);
+
+// begin9
+// let x1 = +prompt("x1=");
+// let x2 = +prompt("x2=");
+// let y1 = +prompt("y1=");
+// let y2 = +prompt("y2=");
+// let L = ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 2;
+// console.log(L);
+
+// begin10
+// let a = +prompt("a=");
+// let b = +prompt("b=");
+// let c = +prompt("c=");
+// let k1 = +b;
+// let k2 = +c;
+// let k3 = +a;
+// console.log((a = k1));
+// console.log((b = k2));
+// console.log((c = k3));
+
+// begin11
+// let x = +prompt("x=");
+// let y = 4 * (x - 3) ** 6 - (7 * (x - 3) ** 3 + 2);
+// console.log(y);
+
+// // begin12
+// let a = +prompt("a=");
+// let a1 = a ** 3;
+// let a2 = a ** 6;
+// let a3 = a ** 9;
+// let a4 = a ** 15;
+// console.log(a1);
+// console.log(a2);
+// console.log(a3);
+// console.log(a4);
+
+// begin13
+
+// let tf = +prompt("tf=");
+// let tc = ((tf - 32) * 5) / 2;
+// console.log(tc);
+
+// begin14
+// let x = +prompt("x=");
+// let y = +prompt("y=");
+// let A = +x;
+// let B = +y;
+// let d = A - B;
+// console.log(d);
+
+// // begin15
+// let x = +prompt("x=");
+// let y = +prompt("y=");
+// let A = +x;
+// let B = +y;
+// let sum = A + B;
+// console.log(sum);
+
+// begin16
+
+// let abc = +prompt("abc=");
+// let bc = abc % 100;
+// let c = bc % 10;
+// let b = (bc - c) / 10;
+// console.log(c);
+// console.log(b);
+
+// begin17
+
+// let abc = +prompt("abc=");
+// let bc = abc % 100;
+// let a = (abc - bc) / 100;
+// let c = bc % 10;
+// let b = (bc - c) / 10;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(a + b + c);
+
+// begin18
+
+// let abc = +prompt("abc=");
+// let bc = abc % 100;
+// let a = (abc - bc) / 100;
+// let c = bc % 10;
+// let b = (bc - c) / 10;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(c * 100 + b * 10 + a);
+
+// begin 19
+
+// let abc = +prompt("abc=");
+// let bc = abc % 100;
+// let a = (abc - bc) / 100;
+// let c = bc % 10;
+// let b = (bc - c) / 10;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(b * 100 + a * 10 + c);
+
+// begin20
+// let abc = +prompt("abc=");
+// let bc = abc % 100;
+// let a = (abc - bc) / 100;
+// let c = bc % 10;
+// let b = (bc - c) / 10;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(a * 100 + c * 10 + b);
+
+// begin21
+
+// let abcdef = +prompt("abcdef=");
+// let cdef = abcdef % 10000;
+// let def = cdef % 1000;
+// let d = (cdef - def) / 1000;
+// console.log(d);
+
+// begin22
+
+// let n = +prompt("n=");
+// let s = n % 3600;
+// let h = (n - s) / 3600;
+// let m = (s - (s % 60)) / 60;
+// console.log(m);
+
+// begin 23 24 25
+
+// let n = +prompt("n=");
+// let r = n % 3600;
+// let h = (n - r) / 3600;
+// let s = r % 60;
+// let m = (r - s) / 60;
+// console.log(h);
+// console.log(m);
+// console.log(s);
